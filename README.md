9ac4394d9272d15e29bdbbf87c9f1a1fe0605a97
rasho22 <ab_rasho@yahoo.fr>
2016-07-06 17:28:48 +0200

    Mon Travail du 6/07/2016

---
+++ README.md
@@ -0,0 +1,4 @@
+##Javascript - Exercices 02
+**Important**  
+Il faut lancer le fichier SpecRunner.html pour valider ses réponses.  
+Les réponses seront à écrire dans le fichier questions.js
---
+++ questions.js
@@ -0,0 +1,51 @@
+/**
+ * Exercice sur les chaines de caractères.
+ * Trouvez la façon de faire la plus optimal.
+ * Il peut y avoir plusieur façon de faire.
+ */
+
+/**
+ * Exercices sur les nombres et les caluls mathématiques
+ */
+
+function tailleString(toto){
+	var result = (toto).length;
+	return result;
+};
+
+function remplaceCar(change, avant, apre){
+	var result = change.replace(avant, apre);
+	return result;
+};
+
+function concatString(textun , textdeux){
+var result= (textun + textdeux);
+return result;
+};
+
+function afficherCar(cinq, n){
+ var select = cinq.charAt(n-1);
+ return select;
+};
+
+function afficherNCar (neufpremiers, n){
+var result= neufpremiers.slice(0,n);
+return result;
+};
+
+function majusculeString(maj){
+var result= maj.toUpperCase();
+return result;
+};
+
+function SupprEspaceString(suprimer){
+var result= suprimer.trim();
+return result;
+};
+function IsString(chaines){
+	var result = isNaN();
+		return result;
+	};
+
+
+
