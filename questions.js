f97dad92b8c83e95dc3917f15ca0815c236bd883
rasho22 <ab_rasho@yahoo.fr>
2016-07-08 15:18:20 +0200

    finalisation de exer.

--- questions.js
+++ questions.js
@@ -43,9 +43,40 @@ var result= suprimer.trim();
 return result;
 };
 function IsString(chaines){
-	var result = isNaN();
-		return result;
-	};
+	if (typeof chaines === 'string'){
+		return true;
+	} 
+	else {
+		return false;
+	}
+};
+
+function AfficherExtensionString (extension){
+	 return extension.substr(extension.lastIndexOf(".") + 1);
+	
+}
+AfficherExtensionString('images/photo01.jpg');
 
 
+function NombreEspaceString (n_espace){
 
+	var result= n_espace.split(' ').length - 1;
+	return result;
+}
+
+//bonus:
+
+
+function calculPuissance (x, y){
+	var result=Math.pow (x, y);
+	return result;
+};
+
+function valeurAbsolue (z){
+	var result= Math.abs(z);
+	return result;
+};
+
+function valeurAbsolueArray (tab){
+	return result= tab.map(valeurAbsolue);
+}
